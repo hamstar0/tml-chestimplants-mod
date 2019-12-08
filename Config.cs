99c2e078afa94c3ecab3c6cecbc80ed2c7527572
hamstar0 <webutek@gmail.com>
2019-12-07 22:22:46 -0800

    v1.1.0 - Implement proper merging of config fields via OverlayChanges

--- Config.cs
+++ Config.cs
@@ -1,70 +1,15 @@
-﻿using HamstarHelpers.Classes.UI.ModConfig;
-using HamstarHelpers.Services.Configs;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using Terraria.ID;
 using Terraria.ModLoader.Config;
+using HamstarHelpers.Classes.UI.ModConfig;
+using HamstarHelpers.Helpers.DotNET.Extensions;
+using HamstarHelpers.Services.Configs;
 
 
 namespace ChestImplants {
 	class MyFloatInputElement : FloatInputElement { }
-
-
-
-
-	public class ChestImplanterSetDefinition : List<ChestImplanterDefinition> {
-		public float TotalWeight() {
-			return this?.Sum( def => def.Weight )
-					?? 0f;
-		}
-	}
-
-
-
-	public class ChestImplanterDefinition {
-		[Range( 0f, 1f )]
-		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
-		public float Weight { get; set; }
-		public HashSet<string> ChestTypes { get; set; }
-		public List<ChestImplanterItemDefinition> ItemDefinitions { get; set; }
-	}
-
-
-
-
-	public class ChestImplanterItemDefinition {
-		public ItemDefinition ChestItem { get; set; }
-
-		[DefaultValue( -1 )]
-		public int WallId { get; set; } = -1;
-
-		[Range( 0f, 1f )]
-		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
-		public float ChancePerChest { get; set; } = 1f;
-
-		[Range( 0, 999 )]
-		[DefaultValue( 1 )]
-		public int MinQuantity { get; set; } = 1;
-
-		[Range( 0, 999 )]
-		[DefaultValue( 1 )]
-		public int MaxQuantity { get; set; } = 1;
-
-		[DefaultValue( 0 )]
-		public int Prefix { get; set; } = 0;
-
-
-
-		////////////////
-
-		public override string ToString() {
-			return "Implanter "
-				+this.MinQuantity+":"+this.MaxQuantity
-				+" "+this.ChestItem.ToString()+"@"+this.ChancePerChest.ToString("N2");
-		}
-	}
 
 
 
@@ -133,6 +78,26 @@ namespace ChestImplants {
 					}
 				} }
 			};
+
+
+
+		////////////////
+
+		public override void OverlayChanges( StackableModConfig changes ) {
+			var mychanges = (ChestImplantsConfig)changes;
+
+			foreach( (string name, ChestImplanterSetDefinition implanterSet) in mychanges.AllFromSetChestImplanterDefinitions ) {
+				this.AllFromSetChestImplanterDefinitions[name] = implanterSet;
+			}
+			foreach( (string name, ChestImplanterSetDefinition implanterSet) in mychanges.RandomPickFromSetChestImplanterDefinitions ) {
+				this.AllFromSetChestImplanterDefinitions[name] = implanterSet;
+			}
+
+			mychanges.AllFromSetChestImplanterDefinitions = this.AllFromSetChestImplanterDefinitions;
+			mychanges.RandomPickFromSetChestImplanterDefinitions = this.RandomPickFromSetChestImplanterDefinitions;
+
+			base.OverlayChanges( changes );
+		}
 	}
 }
 
---
+++ Config_Defs.cs
@@ -0,0 +1,64 @@
+﻿using HamstarHelpers.Classes.UI.ModConfig;
+using HamstarHelpers.Services.Configs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Terraria.ID;
+using Terraria.ModLoader.Config;
+
+
+namespace ChestImplants {
+	public class ChestImplanterSetDefinition : List<ChestImplanterDefinition> {
+		public float TotalWeight() {
+			return this?.Sum( def => def.Weight )
+					?? 0f;
+		}
+	}
+
+
+
+	public class ChestImplanterDefinition {
+		[Range( 0f, 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float Weight { get; set; }
+		public HashSet<string> ChestTypes { get; set; }
+		public List<ChestImplanterItemDefinition> ItemDefinitions { get; set; }
+	}
+
+
+
+
+	public class ChestImplanterItemDefinition {
+		public ItemDefinition ChestItem { get; set; }
+
+		[DefaultValue( -1 )]
+		public int WallId { get; set; } = -1;
+
+		[Range( 0f, 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float ChancePerChest { get; set; } = 1f;
+
+		[Range( 0, 999 )]
+		[DefaultValue( 1 )]
+		public int MinQuantity { get; set; } = 1;
+
+		[Range( 0, 999 )]
+		[DefaultValue( 1 )]
+		public int MaxQuantity { get; set; } = 1;
+
+		[DefaultValue( 0 )]
+		public int Prefix { get; set; } = 0;
+
+
+
+		////////////////
+
+		public override string ToString() {
+			return "Implanter "
+				+this.MinQuantity+":"+this.MaxQuantity
+				+" "+this.ChestItem.ToString()+"@"+this.ChancePerChest.ToString("N2");
+		}
+	}
+}
+
