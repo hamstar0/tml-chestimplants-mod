125e5a2a31b105da0832460cab8b81ac111fdc43
hamstar0 <webutek@gmail.com>
2020-04-15 10:19:08 -0700

    v1.1.1.2 - Fixed config instructions

--- Config.cs
+++ Config.cs
@@ -25,11 +25,11 @@ namespace ChestImplants {
 
 		////////////////
 
-		[Label( "Chest implanters define a list of items to add or remove (via. negative stack size) from chests."
+		[Header( "Chest implanters define a list of items to add or remove (via. negative stack size) from chests."
 			+"\n \nUse the random lists options for random implanters to be picked (as opposed to random chance per item)."
 			+"\n \nItem implanters can specify their item's likeliness of being implanted/extracted."
 			+"\n \nItem implanters choose their quantity between a minimum and maximum value."
-			+"\n \n \n \n" )]
+			+"\n \n \n " )]
 		public bool DebugModeInfo = false;
 		
 		public bool DebugModeVerboseInfo = false;
